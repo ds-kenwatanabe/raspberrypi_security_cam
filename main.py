fa635e672c400a518bac46ba56f5134b0df0d859
ken watanabe <ollhoffchristian@gmail.com>
2023-12-01 10:40:35 -0300

    Added frame_delta to compare subsequent frames to first frame and detect motion

--- main.py
+++ main.py
@@ -7,24 +7,39 @@ class LiveCamera:
 
     def start_capture(self):
         cap = cv2.VideoCapture(self.http)
+        first_frame = None
 
         while True:
             ret, frame = cap.read()
+            text = "No movement"
 
             if not ret:
                 break
 
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             gray_frame = cv2.GaussianBlur(gray_frame, (61, 61), 0)
-            _, threshold = cv2.threshold(gray_frame, 30, 255, cv2.THRESH_BINARY)
+
+            # If the firts frame os None, initialize it
+            if first_frame is None:
+                first_frame = gray_frame
+                continue
+
+            # Compute difference between current and first frame
+            frame_delta = cv2.absdiff(first_frame, gray_frame)
+            _, threshold = cv2.threshold(frame_delta, 30, 255, cv2.THRESH_BINARY)
+            # Find contours on the thresholded image
             contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+            # Loop contours
             for contour in contours:
                 if cv2.contourArea(contour) < 700:
                     continue
                 x, y, w, h = cv2.boundingRect(contour)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                text = "Movement"
 
+            cv2.putText(img=frame, text="Status: {}".format(text), org=(30, 20),
+                        fontFace=0, fontScale=0.8, color=(0, 0, 255), thickness=3)
             cv2.imshow('Motion Detection', frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
