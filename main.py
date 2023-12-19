761613505e595522340181f424880916c7d272c1
ken watanabe <ollhoffchristian@gmail.com>
2023-12-19 14:12:26 -0300

    Updating parameters, adding date and time and saving output

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import cv2
 
 
@@ -11,13 +12,18 @@ class LiveCamera:
         # Read initial frame for comparison
         ret_init, frame_init = cap.read()
         gray_frame_init = cv2.cvtColor(frame_init, cv2.COLOR_BGR2GRAY)
-        gray_frame_init = cv2.GaussianBlur(gray_frame_init, (61, 61), 0)
-        cv2.imshow('Win1', frame_init)
+        gray_frame_init = cv2.GaussianBlur(gray_frame_init, (91, 91), 0)
+
+        # Video writer setup
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        out = cv2.VideoWriter('cam_output.mp4', fourcc, 30.0, (frame_init.shape[1], frame_init.shape[0]))
 
         while True:
             ret, frame = cap.read()
-            text = "No movement"
-
+            # date and time display text
+            date = str(datetime.datetime.now())
+            # Text if no movement is detected
+            text = f"No movement {date}"
             if not ret:
                 break
 
@@ -26,7 +32,7 @@ class LiveCamera:
 
             # Compute difference between current and first frame
             frame_delta = cv2.absdiff(gray_frame_init, gray_frame)
-            _, threshold = cv2.threshold(frame_delta, 30, 255, cv2.THRESH_BINARY)
+            _, threshold = cv2.threshold(frame_delta, 100, 255, cv2.THRESH_BINARY)
             threshold = cv2.dilate(threshold, None)
             # Find contours on the thresholded image
             contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -37,7 +43,10 @@ class LiveCamera:
                     continue
                 x, y, w, h = cv2.boundingRect(contour)
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                text = "Movement"
+                text = f"Movement {date}"
+
+                # Save output
+                out.write(frame)
 
             cv2.putText(img=frame, text="Status: {}".format(text), org=(30, 20),
                         fontFace=0, fontScale=0.8, color=(0, 0, 255), thickness=3)
@@ -47,11 +56,12 @@ class LiveCamera:
                 break
 
         cap.release()  # Release the video capture object
+        out.release()  # Release video writer
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    http_url = 'http://192.168.68.114:8080'
+    http_url = ''
 
     run = LiveCamera(http_url)
     run.start_capture()
-    cv2.destroyAllWindows()
